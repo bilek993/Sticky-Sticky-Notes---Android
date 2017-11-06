1da662bf12ea30b3b8697851922a2b25c5c93461
DESKTOP-03RQ3MJ\jbilińskipl <jbilinskipl@gmail.com>
2017-11-06 17:43:59 +0100

    Added animation for landing activity.

---
+++ app/src/main/java/com/jakubbilinski/stickystickynotesandroid/ImageViewLoopOpacityAnimator.java
@@ -0,0 +1,43 @@
+package com.jakubbilinski.stickystickynotesandroid;
+
+import android.animation.ValueAnimator;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+
+/**
+ * Created by jbili on 06.11.2017.
+ */
+
+public class ImageViewLoopOpacityAnimator {
+
+    private ImageView imageViewAnimated;
+    private ValueAnimator animation;
+
+    public ImageViewLoopOpacityAnimator(ImageView imageViewAnimated, long duration) {
+        this.imageViewAnimated = imageViewAnimated;
+
+        animation = ValueAnimator.ofFloat(0f, 1f);
+        animation.addUpdateListener((animation) -> {
+            imageViewAnimated.setAlpha((float) animation.getAnimatedValue());
+        });
+        animation.setDuration(duration);
+        animation.setRepeatCount(Animation.INFINITE);
+        animation.setRepeatMode(ValueAnimator.REVERSE);
+    }
+
+    public void hideImage() {
+        imageViewAnimated.setAlpha(0f);
+    }
+
+    public void showImage() {
+        imageViewAnimated.setAlpha(1f);
+    }
+
+    public void playAnimation() {
+        animation.start();
+    }
+
+    public void stopAnimation() {
+        animation.cancel();
+    }
+}
--- app/src/main/java/com/jakubbilinski/stickystickynotesandroid/LandingActivity.java
+++ app/src/main/java/com/jakubbilinski/stickystickynotesandroid/LandingActivity.java
@@ -2,12 +2,31 @@ package com.jakubbilinski.stickystickynotesandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class LandingActivity extends AppCompatActivity {
+
+    @BindView(R.id.imageViewIconBackground)
+    ImageView imageViewBackgroundIcon;
+
+    private ImageViewLoopOpacityAnimator backgroundAnimator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+
+        ButterKnife.bind(this);
+
+        playBackgroundAnimation();
+    }
+
+    private void playBackgroundAnimation() {
+        backgroundAnimator = new ImageViewLoopOpacityAnimator(imageViewBackgroundIcon, 1000);
+        backgroundAnimator.hideImage();
+        backgroundAnimator.playAnimation();
     }
 }
