d059228bac859de475d9e8783e996a1aaf352ac9
DESKTOP-03RQ3MJ\jbilińskipl <jbilinskipl@gmail.com>
2017-11-10 17:55:41 +0100

    Added empty activity for notes.

---
+++ app/src/main/java/com/jakubbilinski/stickystickynotesandroid/activities/NotesActivity.java
@@ -0,0 +1,15 @@
+package com.jakubbilinski.stickystickynotesandroid.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jakubbilinski.stickystickynotesandroid.R;
+
+public class NotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notes);
+    }
+}
